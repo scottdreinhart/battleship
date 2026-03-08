12c8008aa97b3ad6e90a08e727024c51c57f912b
Scott Reinhart <scottdreinhart@users.noreply.github.com>
2026-03-08 14:29:21 -0400

    initial scaffold: React + Vite + TypeScript + Electron + Capacitor

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Battleship</h1>
+      <p>Place ships on a grid, take turns guessing opponent locations</p>
+    </div>
+  )
+}
